9f4d2e7777c1839f85d779bb51b1d31fa01d72be
emilianospinoso <emilianospinoso@gmail.com>
2022-04-12 20:14:21 -0300

    clase javascript version 1

---
+++ script.js
@@ -0,0 +1,68 @@
+//Esto es un alert
+//alert("Hola mundo")
+
+/*
+todo
+es 
+comentario
+*/
+
+let  alumno = ""  //creo la variable
+let  nota = 9
+
+const entidad = "Gobierno ciudad"
+//entidad = "nacion"
+
+console.log( "el nombre del alumno es " +   alumno + "y su nota es " + nota) //la imprimo
+
+
+//VAN
+let nombreApellido = "juan batman"
+let User = "juan carlos"
+let _usuario
+let $user
+let nombre_apellido = "Snake case"
+
+//no vannnnnnnnnnnnnnn
+/*let 1ernombre
+let nombre-apellido
+let var super break this*/
+
+
+//////STRING
+let str1 = "JUAN"
+let str2 = 'pEDRO'
+let str3 = `rAMIRO`
+
+let str4 =   str1 + " y " +str2
+let str5 = `${str1} y ${str2}`
+console.log (str4)
+console.log (str5)
+
+//////NUMBER
+
+let num1 = parseFloat(prompt("mete el primer numero"))
+let num2 = parseFloat(prompt("mete el segundo numero"))
+
+//operaciones basicas
+let suma = num1 + num2
+let resta = num1 - num2
+let mult =  num1 * num2;
+let division = num1/num2
+alert("La suma es :"+suma)
+alert("La resta es :"+resta)
+alert("La multiplicacion es :"+mult)
+
+document.write("<h1> Javascript </h1>" + mult );
+
+
+
+
+
+
+
+
+
+
+
+
